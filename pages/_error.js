20e691a9b2ed391af9de7110aed290ab81b7e946
Holben888 <holben888@gmail.com>
2018-08-06 14:59:54 -0400

    Sentry set to use local env variables

--- pages/_error.js
+++ pages/_error.js
@@ -3,7 +3,7 @@ import IsomorphicRaven from '../lib/raven';
 
 class MyError extends NextError {
   static async getInitialProps(context) {
-    if (context.err && process.env.NODE_ENV === 'production') {
+    if (context.err && process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN) {
       // Log error to Sentry
       IsomorphicRaven.captureException(context.err)
     }
