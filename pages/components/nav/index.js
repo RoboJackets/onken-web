bb5b4890290f36ca81fe3ba4317638abc270d037
Holben888 <holben888@gmail.com>
2018-08-08 21:42:32 -0400

    Sidenav now adaptable to screen size on initial render

--- pages/components/nav/index.js
+++ pages/components/nav/index.js
@@ -1,45 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Sidenav from './sidenav'
 import Header from './header'
-import { withTheme } from 'styled-components'
 
-class Nav extends Component {
-  constructor() {
-    super()
-    this.state = {
-      expanded: true,
-    }
-  }
-  componentDidMount = () => {
-    const onWindowResize = () => {
-      const { expanded } = this.state
-      const mediaQuery = window.matchMedia(`(max-width: ${this.props.theme.sidenavWidth * 3}em)`)
-      if (mediaQuery.matches && expanded || !mediaQuery.matches && !expanded)
-        this.onMenuClicked()
-    }
-    window.addEventListener('resize', onWindowResize)
-    onWindowResize()
-  }
-
-  onMenuClicked = () => {
-    this.setState({
-      expanded: !this.state.expanded,
-    })
-  }
-  render() {
-    const { expanded } = this.state
-    return (
-      <React.Fragment>
-        <Header expanded={expanded} onMenuClicked={this.onMenuClicked} />
-        <Sidenav expanded={expanded} />
-      </React.Fragment>
-    )
-  }
-}
+const Nav = ({ showNav, onToggleNav }) => (
+  <React.Fragment>
+    <Header expanded={showNav} onMenuClicked={onToggleNav} />
+    <Sidenav expanded={showNav} />
+  </React.Fragment>
+)
 
 Nav.propTypes = {
-  theme: PropTypes.object.isRequired,
+  showNav: PropTypes.bool.isRequired,
+  onToggleNav: PropTypes.func.isRequired,
 }
 
-export default withTheme(Nav)
+export default Nav
