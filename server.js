c07147303be2d8acc40e074dab3ae8d6ab05507f
Holben888 <holben888@gmail.com>
2018-08-09 11:16:08 -0400

    http-proxy-middleware added, server.js configured

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 const express = require('express')
 const next = require('next')
 
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({
+  dir: '.',
+  dev,
+})
+
+const devProxy = {
+  '/api': {
+    target: 'https://localhost:5000/api/',
+    pathRewrite: { '^/api': '/' },
+    changeOrigin: true
+  }
+}
+
 const handle = app.getRequestHandler()
 
 app.prepare()
   .then(() => {
     const server = express()
 
+    if (dev) {
+      const proxyMiddleware = require('http-proxy-middleware')
+      Object.keys(devProxy).forEach(context => (
+        server.use(proxyMiddleware(context, devProxy[context]))
+      ))
+    }
+
     server.get('*', (req, res) => {
       return handle(req, res)
     })
 
-    server.listen(3000, (err) => {
+    server.listen(port, (err) => {
       if (err) throw err
     })
   })
