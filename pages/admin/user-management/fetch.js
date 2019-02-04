817ae0762664596d16a2f0643f1acc7f303a2920
Holben888 <holben888@gmail.com>
2019-02-04 16:57:19 -0500

    update lockfile

--- pages/admin/user-management/fetch.js
+++
@@ -1,7 +0,0 @@
-import { resHandler } from '../../fetch-base'
-import fetch from 'isomorphic-unfetch'
-import * as testData from '../test-data.json'
-
-export const fetchTableData = async () => testData.default
-
-export const fetchTest = async () => resHandler(await fetch('https://api.tvmaze.com/search/shows?q=batman'))
